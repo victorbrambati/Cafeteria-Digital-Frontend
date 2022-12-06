6b82f64fc8a0913d4a52d52b8337f75387843786
victorbrambati <victorbrambati@gmail.com>
2022-12-05 22:53:31 -0300

    feat: create a header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,7 @@ import {
   Tag,
   Tooltip,
   useBreakpointValue,
+  Image,
 } from "@chakra-ui/react";
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { RiShoppingCartLine } from "react-icons/ri";
@@ -16,15 +17,54 @@ import Link from "next/link";
 import { BackgroundProps } from "@chakra-ui/react";
 import { useCartStore } from "../store/useCart";
 import { useEffect, useState } from "react";
+import multiavatar from "@multiavatar/multiavatar";
+import { useAuth } from "../store/useAuth";
+
 export default function Header({ backgroundColor }: BackgroundProps) {
-  let newTotalProducts = useCartStore((state) => state.totalProducts);
+  let actualTotalProducts = useCartStore((state) => state.totalProducts);
 
-  useCartStore((state) => state.totalProducts);
-  const [totalProducts, setTotalProducts] = useState(newTotalProducts);
+  const actualToken = useAuth((state) => state.token);
+ const actualUserInfo = useAuth((state) => state.userInfo);
 
-  useEffect(() => setTotalProducts(newTotalProducts), [newTotalProducts]);
-
+  const [totalProducts, setTotalProducts] = useState(actualTotalProducts);
+  const [token, setToken] = useState(actualToken);
+  const [userInfo, setUserInfo] = useState(actualUserInfo);
   const fontSizeHeading = useBreakpointValue({ base: "4xl", md: "5xl" });
+
+
+  useEffect(() => setTotalProducts(actualTotalProducts), [actualTotalProducts]);
+  useEffect(() => {setToken(actualToken), setUserInfo(actualUserInfo)}, [actualToken, actualUserInfo]);
+
+  const REGEX = {
+    whitespace: /\s+/g,
+    urlHexPairs: /%[\dA-F]{2}/g,
+    quotes: /"/g,
+  };
+
+  function specialHexEncode(match: string) {
+    switch (match) {
+      case "%20":
+        return " ";
+      case "%3D":
+        return "=";
+      case "%3A":
+        return ":";
+      case "%2F":
+        return "/";
+      default:
+        return match.toLowerCase();
+    }
+  }
+
+  const toDataURI = (string: string) =>
+    `data:image/svg+xml,${encodeURIComponent(string)
+      .trim()
+      .replace(REGEX.whitespace, " ")
+      .replace(REGEX.quotes, "'")
+      .replace(REGEX.urlHexPairs, specialHexEncode)}`;
+
+  const svg = toDataURI(multiavatar(String(userInfo.id)));
+
   return (
     <Box>
       <Stack width="100%" height="120px" backgroundColor={backgroundColor}>
@@ -75,19 +115,25 @@ export default function Header({ backgroundColor }: BackgroundProps) {
                 </Button>
               </Tooltip>
             </Link>
+            <Link href={token === "" ? "/login" : "/user"}>
+              <Tooltip alignItems="center" label="Minha conta">
+                <Button colorScheme="transparent" size={"56"}>
+                  <Box
+                    color="black"
+                    _hover={{
+                      color: backgroundColor === "white" ? "blue.400" : "white",
+                    }}
+                  >
+                    {token === "" ?  (
+                      <HiOutlineUserCircle color="#E5F4FD" size={"40"} />
+                    ) : (
+                      <Image src={svg} width="40px" height="40px" />
+                    )}
+                  </Box>
+                </Button>
+              </Tooltip>
+            </Link>
 
-            <Tooltip alignItems="center" label="Minha conta">
-              <Button colorScheme="transparent" size={"56"}>
-                <Box
-                  color="black"
-                  _hover={{
-                    color: backgroundColor === "white" ? "blue.400" : "white",
-                  }}
-                >
-                  <HiOutlineUserCircle size={"40"} />
-                </Box>
-              </Button>
-            </Tooltip>
             {/* <Button colorScheme="transparent" size={"56"}>
            <Box color="black" _hover={{color: "white"}}>
             fazer login
