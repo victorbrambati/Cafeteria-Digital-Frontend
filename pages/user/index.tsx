2745ec898c11475ca200d78fb09e3910ce7396bd
victorbrambati <victorbrambati@gmail.com>
2022-12-05 22:54:32 -0300

    feat: create user pages

---
+++ pages/user/index.tsx
@@ -0,0 +1,45 @@
+import { Box, Stack, Text, HStack, Heading, Button, useBreakpointValue, useDisclosure } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import CartItem from '../../components/CartItem'
+import Header from '../../components/Header'
+import ConfirmPurchase from '../../components/ConfirmPurchase'
+import { useAuth } from '../../store/useAuth'
+import { CoffeeProductType } from "../../data/products";
+import Router from 'next/router'
+import React from 'react'
+
+export default function User() {
+  const actualUserInfo = useAuth(state => state.userInfo);
+
+  const [userInfo, setUserInfo] = useState(actualUserInfo);
+
+  React.useEffect(() => {
+    setUserInfo(actualUserInfo);
+  }, [actualUserInfo]);
+
+  if(userInfo.email === ''){
+    Router.push("/login");
+  }
+  
+  
+
+  const paddingLeft = useBreakpointValue({ base: "0", md: "25" });
+  const minH = useBreakpointValue({ base: "350", md: "550" });
+  
+
+  return (<Box backgroundColor="black" height="100vh" >
+      <Header backgroundColor="white" />
+
+    <Stack alignItems="center" flexDirection={'column'}>
+      <Heading color="white" fontSize={52}  marginTop={10} marginBottom={15}>Minha conta.</Heading>
+
+      <Stack alignItems="center" w={1200} paddingLeft={paddingLeft} paddingTop={paddingLeft}>
+      <Heading color="white">Email: {userInfo.email}</Heading>
+      <Heading color="white">ID: {userInfo.id}</Heading>
+
+      </Stack>
+    </Stack>
+    </Box>
+
+  )
+}
