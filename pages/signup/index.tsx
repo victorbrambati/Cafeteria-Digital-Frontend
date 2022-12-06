3fbaaf3b75d23aba9f2738897c4c65d1769bb3a4
victorbrambati <victorbrambati@gmail.com>
2022-12-05 22:53:51 -0300

    feat: add singup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,4 +1,8 @@
 import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
   Box,
   Button,
   Checkbox,
@@ -15,13 +19,25 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import Link from 'next/link'
+import Router  from 'next/router'
 import * as React from 'react'
 import { Logo } from '../../components/Logo'
 import { OAuthButtonGroup } from '../../components/OAuthButtonGroup'
 import { PasswordField } from '../../components/PasswordField'
+import { useAuth } from '../../store/useAuth'
 
 
 export default function index(){
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const signUp = useAuth(state => state.signUp);
+  const success = useAuth(state => state.signUpSuccess);
+
+  if(success){
+    Router.push("/login");
+  }
+
 return (
 <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
     <Stack spacing="4">
@@ -50,18 +66,18 @@ return (
           <Stack spacing="5">
             <FormControl>
               <FormLabel htmlFor="email">Email</FormLabel>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" onChange={e => setEmail(e.target.value)} />
             </FormControl>
-            <PasswordField />
+            <PasswordField onChange={e => setPassword(e.target.value)} />
           </Stack>
           <Stack spacing="6" >
-            <Button colorScheme='blue'>Criar uma conta</Button>
+            <Button  onClick={() => {signUp(email, password)}}  colorScheme='blue'>Criar uma conta</Button>
             <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
           <HStack spacing="1" justify="center">
             <Text color="muted">Já possuí uma conta?</Text>
             <Link href="/login">
             <Button variant="link" colorScheme="blue" >
-              fazer login
+              fazer login 
             </Button>
             </Link>
           </HStack>
@@ -76,6 +92,13 @@ return (
         </Stack>
       </Box>
     </Stack>
+    { success ?
+      <Alert status='error'>
+  <AlertIcon />
+  <AlertTitle>Email ou senha podem não cumprir os requisitos!</AlertTitle>
+  <AlertDescription>mínimo 8 caracteres, letra maíuscula, minuscula e caracteres especiais</AlertDescription>
+</Alert> : <div></div>
+    }
   </Container>
 )
 }
